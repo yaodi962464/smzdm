6cda37a8033ee5471b69394fb4925138f9cb8ae2
yaod <445659342@qq.com>
2020-03-06 15:34:56 +0800

    新增

---
+++ start.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Time    : 2020-03-06 15:19
+Author  : Yaodi
+Object  : 
+"""
+from zhangdama.start import get_geetest_challenge
+
+if __name__ == '__main__':
+    get_geetest_challenge(username="yaorUserName", password="yourPassword")
